bd8f9c8af530c5c5d0ef4533949822aa6b738d0c
sohamxda7 <sensoham135@gmail.com>
2021-05-05 06:21:18 +0000

    lavender : update to latest coral fp
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -36,6 +36,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := lavender
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V11.0.1.0.QFGMIXM release-keys"
+    PRIVATE_BUILD_DESC="coral-user 11 RQ2A.210505.002 7246365 release-keys"
 
-BUILD_FINGERPRINT :="google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys"
+BUILD_FINGERPRINT :="google/coral/coral:11/RQ2A.210505.002/7246365:user/release-keys"
