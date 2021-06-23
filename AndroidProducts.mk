0720881f1d0aa82d5ad9f8804b949fd5a0a5dfb5
sohamxda7 <sensoham135@gmail.com>
2021-06-23 13:08:00 +0000

    lavender : bringup cafex
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lavender.mk
+    $(LOCAL_DIR)/cafex_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lavender-user \
-    aosp_lavender-userdebug \
-    aosp_lavender-eng
+    cafex_lavender-user \
+    cafex_lavender-userdebug \
+    cafex_lavender-eng
