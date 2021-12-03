5c014fde2b0975b338e2e9a0b488c4f858ec6c44
sohamxda7 <sensoham135@gmail.com>
2021-12-03 08:07:47 +0000

    Revert "lavender : switch to coral october spl"
    This reverts commit 525585a9eebe80e3124b5e303e2e54d6ef896662.
    
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- bliss_lavender.mk
+++ bliss_lavender.mk
@@ -27,6 +27,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lavender" \
     PRODUCT_NAME="lavender" \
-    PRIVATE_BUILD_DESC="coral-user 12 SP1A.210812.015 7679548 release-keys"
+    PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V11.0.5.0.PFGMIXM release-keys"
 
-BUILD_FINGERPRINT :="google/coral/coral:12/SP1A.210812.015/7679548:user/release-keys"
+BUILD_FINGERPRINT :="google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
